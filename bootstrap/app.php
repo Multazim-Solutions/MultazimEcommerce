3242582fcfed8abcfb46231158a7df9d8727606f
Naimul Kabir <kabirnayeem.99@gmail.com>
2026-02-06 14:40:48 +0600

    feat[P1-T03]: add request id middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->appendToGroup('web', [
+            \App\Http\Middleware\RequestId::class,
+        ]);
+
+        $middleware->appendToGroup('api', [
+            \App\Http\Middleware\RequestId::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
