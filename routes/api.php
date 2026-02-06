16c065a821014229412e4b258425f150d3a06442
Naimul Kabir <kabirnayeem.99@gmail.com>
2026-02-06 14:40:24 +0600

    feat[P1-T02]: add api v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::name('api.')
+Route::prefix('v1')
+    ->name('api.')
     ->group(function () {
         Route::get('/health', function (Request $request) {
             return response()->json([
